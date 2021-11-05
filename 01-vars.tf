9efdf5051833693cb85833edad4366e9dc992c17
Сергей <nightywi@gmail.com>
2021-11-05 18:22:32 +0200

    Delete 01-vars.tf

--- 01-vars.tf
+++
@@ -1,3 +0,0 @@
-variable "datadog_api_key" { default = "4c54442bb2f8892e6df3095fa67d7ad0" }
-variable "datadog_app_key" { default = "065deb03fa516290d6a02f3be442dcc2eb663dd5" }
-variable "datadog_api_url" { default = "https://api.datadoghq.eu/" }
